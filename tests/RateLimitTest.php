9887a75194f6590c767683db681d815fa9bd900b
Suhayb Alghutaymil <sohib6@gmail.com>
2019-04-28 22:41:57 +0300

    Apply fixes from StyleCI (#2)

--- tests/RateLimitTest.php
+++ tests/RateLimitTest.php
@@ -3,28 +3,26 @@
  * Created by PhpStorm.
  * User: Sohib
  * Date: 2019-04-24
- * Time: 22:10
+ * Time: 22:10.
  */
 
 namespace Suhayb\RateLimit\Tests;
 
-
 use PHPUnit\Framework\TestCase;
-use Suhayb\RateLimit\Adapters\ArrayAdapter;
 use Suhayb\RateLimit\RateLimit;
+use Suhayb\RateLimit\Adapters\ArrayAdapter;
 
 class RateLimitTest extends TestCase
 {
     /** @test */
     public function it_can_get_an_ip()
     {
-
         $rateLimit = new RateLimit(new ArrayAdapter([
 
         ]));
 
-        $this->assertIsInt($rateLimit->check("10.11.13.1"));
-        $this->assertEquals(0, $rateLimit->check("10.11.13.1"));
+        $this->assertIsInt($rateLimit->check('10.11.13.1'));
+        $this->assertEquals(0, $rateLimit->check('10.11.13.1'));
     }
 
     /** @test */
@@ -34,21 +32,21 @@ class RateLimitTest extends TestCase
 
         ]));
 
-        $rateLimit->store("10.10.1.1", 2);
+        $rateLimit->store('10.10.1.1', 2);
 
-        $this->assertArrayHasKey("10.10.1.1", $rateLimit->all());
-        $this->assertEquals(2, $rateLimit->check("10.10.1.1"));
+        $this->assertArrayHasKey('10.10.1.1', $rateLimit->all());
+        $this->assertEquals(2, $rateLimit->check('10.10.1.1'));
     }
 
     /** @test */
     public function it_can_delete()
     {
         $rateLimit = new RateLimit(new ArrayAdapter([
-            "10.1.1.1" => 40
+            '10.1.1.1' => 40,
         ]));
 
-        $rateLimit->delete("10.1.1.1");
-        $this->assertArrayNotHasKey("10.1.1.1", $rateLimit->all());
-        $this->assertEquals(0, $rateLimit->check("10.10.1.1"));
+        $rateLimit->delete('10.1.1.1');
+        $this->assertArrayNotHasKey('10.1.1.1', $rateLimit->all());
+        $this->assertEquals(0, $rateLimit->check('10.10.1.1'));
     }
 }
